210eb7de6d2479750f1efd9a2dac04151a1785ba
gr691_fnv <gr691_fnv@STUDVM-1>
2021-06-28 10:38:20 +0700

    Настройка логики окна и добавление ресурсов

--- AddEmployee/MainWindow.xaml.cs
+++ AddEmployee/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace AddEmployee
     /// </summary>
     public partial class MainWindow : Window
     {
+        Entities entities = new Entities();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Employee employee = new Employee();
+                employee.FirstName = txtFName.Text;
+                employee.LastName = txtLName.Text;
+                employee.MiddleName = txtMName.Text;
+                if (Convert.ToInt32(cbSpec.SelectedValue) != 0) employee.ID_Competence_FK = Convert.ToInt32(cbSpec.SelectedValue);
+                if (Convert.ToInt32(cbWorkShift.SelectedValue) != 0) employee.ID_WorkShift_FK = Convert.ToInt32(cbWorkShift.SelectedValue);
+                entities.Employee.Add(employee);
+                entities.SaveChanges();
+                dgEmployee.ItemsSource = entities.Employee.ToList();
+                MessageBox.Show("Данные успешно добавлены");
+            }
+            catch
+            {
+                MessageBox.Show("Некорректные данные или ошибка связи с базой данных");
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgEmployee.ItemsSource = entities.Employee.ToList();
+            cbSpec.ItemsSource = entities.Competence.ToList();
+            cbWorkShift.ItemsSource = entities.WorkShift.ToList();
         }
     }
 }
